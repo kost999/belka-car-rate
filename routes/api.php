cd323f87ce026ce3ef5a4727640c8514d730da2e
kost999 <ka@kodix.ru>
2018-07-11 13:08:46 +0300

    routing regexp & json http errors

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@
 Route::namespace('Api')->prefix('1.0.0')->group(function () {
 	Route::prefix('rates')->group(function (){
 		Route::get('/')->name('rates')->uses('RatesController@list');
-		Route::get('/{rate}')->name('rate')->uses('RatesController@item');
+		Route::get('/{rate}')->name('rate')->uses('RatesController@item')->where('rate', '[0-9]+');
 	});
 });
